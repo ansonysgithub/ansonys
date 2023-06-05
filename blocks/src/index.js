f3620beb154e90d29f187ac6ae26cd6ccb5f7fde
Ansony <ansonymendezsuarez@gmail.com>
2023-06-05 16:09:14 -0600

    adding gutenberg ssr

--- blocks/src/index.js
+++ blocks/src/index.js
@@ -1,5 +1,7 @@
 import { registerBlockType } from '@wordpress/blocks';
-import { TextControl } from '@wordpress/components';
+import { InspectorControls } from '@wordpress/block-editor';
+import { TextControl, PanelBody, PanelRow } from '@wordpress/components';
+import ServerSideRender from '@wordpress/server-side-render';
 
 registerBlockType(
     'an/block',
@@ -7,7 +9,7 @@ registerBlockType(
         title: 'Basic Block',
         description: 'Our first block',
         icon: 'smiley',
-        category: 'posts',
+        category: 'products',
         attributes: {
             content: {
                 type: 'string',
@@ -16,16 +18,31 @@ registerBlockType(
         },
         edit: (props) => {
             const { attributes: { content }, setAttributes, className, isSelected } = props;
+
             const handlerOnChangeInput = (newContent) => {
-                setAttributes({ content: newContent });
-            };
-            return (
-                <TextControl
-                    label="Fill the field"
-                    value={content}
-                    onChange={handlerOnChangeInput}
+                setAttributes({ content: newContent })
+            }
+
+            return <>
+                <InspectorControls>
+                    <PanelBody
+                        title="Modify text of Basic Block"
+                        initialOpen={false}
+                    >
+                        <PanelRow>
+                            <TextControl
+                                label="Fill the fields"
+                                value={content}
+                                onChange={handlerOnChangeInput}
+                            />
+                        </PanelRow>
+                    </PanelBody>
+                </InspectorControls>
+                <ServerSideRender
+                    block="an/block"
+                    attributes={props.attributes}
                 />
-            )
+            </>
         },
         save: () => null,
     }
